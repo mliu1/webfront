a07601e8c7abd35d26502525cb5121286ef16c85
mliu1 <mingliu197@gmail.com>
2023-01-16 15:12:02 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title='My Webpage', page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I am Ming :wave:")
+st.title("A data scientist from San Diego")
+st.write("I am passionate about finding ways to build AI powered app")
+st.write("[learn more >](http://linkedin.com/ming_uiuc)")
